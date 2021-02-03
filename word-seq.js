9ac8eea7fe4a9c86f86c9181f3559c4ee5490ee1
marrigamard <marrigamard@simple.com>
2021-02-02 20:02:32 -0800

    basic stdin support

--- word-seq.js
+++ word-seq.js
@@ -5,14 +5,20 @@ const { outputNGramList } = require('./create-ngram-list');
 const arguments = process.argv.slice(2);
 let text = '';
 
-arguments.forEach(item => {
-  try {
-    const data = fs.readFileSync(`${item}`, 'utf8');
-    text += data;
-  } catch (err) {
-    console.error(err)
-  }
-});
+if(arguments.length > 0) {
+  arguments.forEach(item => {
+    try {
+      const data = fs.readFileSync(`${item}`, 'utf8');
+      text += data;
+    } catch (err) {
+      console.error(err)
+    }
+  });
+  const nGrams = outputNGramList(text);
+  console.log(nGrams);
+} else {
+  var data = fs.readFileSync(0, 'utf-8');
+  const nGrams = outputNGramList(data);
+  console.log(nGrams);
+}
 
-const nGrams = outputNGramList(text);
-console.log(nGrams);
